7fea2b5b9cf57957088cfddd332065d1148f3968
Andrew Wilkie <hullpod-central@gmail.com>
2016-05-21 13:45:43 +1000

    Testing propeller motor.

--- iot/Manager.py
+++ iot/Manager.py
@@ -15,20 +15,20 @@ try:
     sws = UltrasonicHCSR04()
     print "Activating Shallow Water Sensor ..."
 
-    counter = 0
-    while counter <=10:
-        sws.activate()
-        print "Depth :", sws.get_depth()
-        counter += 1
+    # counter = 0
+    # while counter <=10:
+    #     sws.activate()
+    #     print "Depth :", sws.get_depth()
+    #     counter += 1
 
-        # prop = DC()
-        # if prop.start_motor():
-        #     print "Motor running for 5 seconds ..."
-        #     time.sleep(5)
-        #     prop.stop_motor()
-        #     gp.cleanup()
-        # else:
-        #     print "Motor NOT running!"
+    prop = DC()
+    if prop.start_motor():
+        print "Motor running for 5 seconds ..."
+        time.sleep(5)
+        prop.stop_motor()
+        gp.cleanup()
+    else:
+        print "Motor NOT running!"
 except:
     print "Unexpected error:", sys.exc_info()[0]
     gp.cleanup()
